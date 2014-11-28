e8b3793a1aa5c817f3c290592c77f1092ea7163e
Lingbo Tang <lingbo19.tang@gmail.com>
2014-11-28 03:20:36 -0700

    Update GetOnePic.java

--- WEB-INF/classes/GetOnePic.java
+++ WEB-INF/classes/GetOnePic.java
@@ -29,8 +29,8 @@ public class GetOnePic extends HttpServlet{
     public void doGet(HttpServletRequest request,
 		      HttpServletResponse response)
 	throws ServletException, IOException {
-        String username = "lingbo";
-        String password = "TlboSci1994";
+        String username = "****";
+        String password = "*****";
         String drivername = "oracle.jdbc.driver.OracleDriver";
         String dbstring ="jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
         
